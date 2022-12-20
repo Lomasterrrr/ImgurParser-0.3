288e9c843079cffce1d2766adf97fb38653d0f27
lomaster <119946584+Lomasterrrr@users.noreply.github.com>
2022-12-20 13:54:51 +0300

    Add files via upload

---
+++ parser/generator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Text; 
+
+namespace parser
+{
+    internal class generator
+    {
+        public int length = 5;
+        public int i = 0;
+        public static string GenerateStr(int length)
+        {
+            const string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            StringBuilder sb = new StringBuilder();
+            Random rnd = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = rnd.Next(chars.Length);
+                sb.Append(chars[index]);
+            }
+            return sb.ToString();
+        }
+        public static string GenerateStrSpeed(int length)
+        {
+            const string chars = "0123456789";
+
+            StringBuilder sb = new StringBuilder();
+            Random rnd = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = rnd.Next(chars.Length);
+                sb.Append(chars[index]);
+            }
+            return sb.ToString();
+        }
+
+        public string GenerateLink(string format) 
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            string g = GenerateStr(length);
+            string result = "https://i.imgur.com/" + g + format;
+            WebClient webcl = new WebClient();
+            webcl.DownloadFile(result, g + format);
+            i++;
+            stopwatch.Stop();
+            Console.WriteLine(" " + result + " | Downloaded for: " + stopwatch.ElapsedMilliseconds + "ms" + "\t-| Count: " + i + "");
+            return result;
+            
+        }
+        public string GenerateLinkSpeed(string format)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            string g = GenerateStrSpeed(length);
+            string result = "https://i.imgur.com/" + g + format;
+            WebClient webcl = new WebClient();
+            webcl.DownloadFile(result, g + format);
+            i++;
+            stopwatch.Stop();
+            Console.WriteLine(" " + result);
+            return result;
+
+        }
+
+
+    }
+}
---
+++ parser/logo.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace parser
+{
+    internal class logo
+    {
+        public void Main()
+        {
+            Console.WriteLine();
+            Thread.Sleep(300);
+            Console.WriteLine("    ██▓     ▒█████   ███▄ ▄███▓ ▄▄▄        ██████ ▄▄▄█████▓▓█████  ██▀███  ");
+            Thread.Sleep(300);
+            Console.WriteLine("   ▓██▒    ▒██▒  ██▒▓██▒▀█▀ ██▒▒████▄    ▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██ ▒ ██▒");
+            Thread.Sleep(300);
+            Console.WriteLine("   ▒██░    ▒██░  ██▒▓██    ▓██░▒██  ▀█▄  ░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██ ░▄█ ▒");
+            Thread.Sleep(300);
+            Console.WriteLine("   ▒██░    ▒██   ██░▒██    ▒██ ░██▄▄▄▄██   ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██▀▀█▄  ");
+            Thread.Sleep(300);
+            Console.WriteLine("   ░██████▒░ ████▓▒░▒██▒   ░██▒ ▓█   ▓██▒▒██████▒▒  ▒██▒ ░ ░▒████▒░██▓ ▒██▒");
+            Thread.Sleep(300);
+            Console.WriteLine("   ░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒░   ░  ░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒▓ ░▒▓░");
+            Thread.Sleep(300);
+            Console.WriteLine("   ░ ░ ▒  ░  ░ ▒ ▒░ ░  ░      ░  ▒   ▒▒ ░░ ░▒  ░ ░    ░     ░ ░  ░  ░▒ ░ ▒░");
+            Thread.Sleep(300);
+            Console.WriteLine("     ░ ░   ░ ░ ░ ▒  ░      ░     ░   ▒   ░  ░  ░    ░         ░     ░░   ░ ");
+            Thread.Sleep(300);
+            Console.WriteLine(" Beta 0.3  ░  ░    ░ ░         ░         ░  ░      ░              ░  ░   ░   ");
+            Thread.Sleep(300);
+            Console.WriteLine(" ---------------------------------------------------------------------------");
+            Console.WriteLine(" [1] - i.imgur.com (normal_speed)");
+            Console.WriteLine(" [2] - i.imgur.com (max_speed)");
+            Console.WriteLine(" [3] - Help");
+            Console.WriteLine(" [4] - Exit");
+            Console.WriteLine(" ---------------------------------------------------------------------------");
+        }
+
+        public void Format() {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("    ██▓     ▒█████   ███▄ ▄███▓ ▄▄▄        ██████ ▄▄▄█████▓▓█████  ██▀███  ");
+            Console.WriteLine("   ▓██▒    ▒██▒  ██▒▓██▒▀█▀ ██▒▒████▄    ▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██ ▒ ██▒");
+            Console.WriteLine("   ▒██░    ▒██░  ██▒▓██    ▓██░▒██  ▀█▄  ░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██ ░▄█ ▒");
+            Console.WriteLine("   ▒██░    ▒██   ██░▒██    ▒██ ░██▄▄▄▄██   ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██▀▀█▄  ");
+            Console.WriteLine("   ░██████▒░ ████▓▒░▒██▒   ░██▒ ▓█   ▓██▒▒██████▒▒  ▒██▒ ░ ░▒████▒░██▓ ▒██▒");
+            Console.WriteLine("   ░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒░   ░  ░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒▓ ░▒▓░");
+            Console.WriteLine("   ░ ░ ▒  ░  ░ ▒ ▒░ ░  ░      ░  ▒   ▒▒ ░░ ░▒  ░ ░    ░     ░ ░  ░  ░▒ ░ ▒░");
+            Console.WriteLine("     ░ ░   ░ ░ ░ ▒  ░      ░     ░   ▒   ░  ░  ░    ░         ░     ░░   ░ ");
+            Console.WriteLine(" Beta 0.3  ░  ░    ░ ░         ░         ░  ░      ░              ░  ░   ░   ");
+            Console.WriteLine(" ---------------------------------------------------------------------------");
+            Console.WriteLine(" [1] - .png  [4] - .all)");
+            Console.WriteLine(" [2] - .jpg  [5] - .custom");
+            Console.WriteLine(" [3] - .gif");
+            Console.WriteLine(" ---------------------------------------------------------------------------");
+        }
+
+        public void Logo() {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("    ██▓     ▒█████   ███▄ ▄███▓ ▄▄▄        ██████ ▄▄▄█████▓▓█████  ██▀███  ");
+            Console.WriteLine("   ▓██▒    ▒██▒  ██▒▓██▒▀█▀ ██▒▒████▄    ▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██ ▒ ██▒");
+            Console.WriteLine("   ▒██░    ▒██░  ██▒▓██    ▓██░▒██  ▀█▄  ░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██ ░▄█ ▒");
+            Console.WriteLine("   ▒██░    ▒██   ██░▒██    ▒██ ░██▄▄▄▄██   ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██▀▀█▄  ");
+            Console.WriteLine("   ░██████▒░ ████▓▒░▒██▒   ░██▒ ▓█   ▓██▒▒██████▒▒  ▒██▒ ░ ░▒████▒░██▓ ▒██▒");
+            Console.WriteLine("   ░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒░   ░  ░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒▓ ░▒▓░");
+            Console.WriteLine("   ░ ░ ▒  ░  ░ ▒ ▒░ ░  ░      ░  ▒   ▒▒ ░░ ░▒  ░ ░    ░     ░ ░  ░  ░▒ ░ ▒░");
+            Console.WriteLine("     ░ ░   ░ ░ ░ ▒  ░      ░     ░   ▒   ░  ░  ░    ░         ░     ░░   ░ ");
+            Console.WriteLine(" Beta 0.3  ░  ░    ░ ░         ░         ░  ░      ░              ░  ░   ░   ");
+            Console.WriteLine(" ---------------------------------------------------------------------------");
+        }
+        public void menuHelp()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("    ██▓     ▒█████   ███▄ ▄███▓ ▄▄▄        ██████ ▄▄▄█████▓▓█████  ██▀███  ");
+            Console.WriteLine("   ▓██▒    ▒██▒  ██▒▓██▒▀█▀ ██▒▒████▄    ▒██    ▒ ▓  ██▒ ▓▒▓█   ▀ ▓██ ▒ ██▒");
+            Console.WriteLine("   ▒██░    ▒██░  ██▒▓██    ▓██░▒██  ▀█▄  ░ ▓██▄   ▒ ▓██░ ▒░▒███   ▓██ ░▄█ ▒");
+            Console.WriteLine("   ▒██░    ▒██   ██░▒██    ▒██ ░██▄▄▄▄██   ▒   ██▒░ ▓██▓ ░ ▒▓█  ▄ ▒██▀▀█▄  ");
+            Console.WriteLine("   ░██████▒░ ████▓▒░▒██▒   ░██▒ ▓█   ▓██▒▒██████▒▒  ▒██▒ ░ ░▒████▒░██▓ ▒██▒");
+            Console.WriteLine("   ░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒░   ░  ░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░  ▒ ░░   ░░ ▒░ ░░ ▒▓ ░▒▓░");
+            Console.WriteLine("   ░ ░ ▒  ░  ░ ▒ ▒░ ░  ░      ░  ▒   ▒▒ ░░ ░▒  ░ ░    ░     ░ ░  ░  ░▒ ░ ▒░");
+            Console.WriteLine("     ░ ░   ░ ░ ░ ▒  ░      ░     ░   ▒   ░  ░  ░    ░         ░     ░░   ░ ");
+            Console.WriteLine(" Beta 0.3  ░  ░    ░ ░         ░         ░  ░      ░              ░  ░   ░   ");
+            Console.WriteLine(" ---------------------------------------------------------------------------");
+            Console.WriteLine(" This program allows you to parse photos from imgur.");
+            Console.WriteLine(" Supports 4 formats, or any of yours.");
+            Console.WriteLine(" It is written in c#.");
+            Console.WriteLine();
+            Console.WriteLine(" Photos are saved in the root folder.");
+            Console.WriteLine(" Output text format: COUNT - URL - STATUS_DOWNLOAD");
+            Console.WriteLine(" Speed: 1m/517 photo.");
+            Console.WriteLine(" ---------------------------------------------------------------------------");
+            Console.WriteLine(" Press any key to exit....");
+            Console.ReadKey();
+            
+        }
+    }
+}
